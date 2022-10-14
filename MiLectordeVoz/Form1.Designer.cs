55c8c889acc82904da7cb094be2ea210db4f96a1
Alumno <Alumno@PC-16>
2022-10-14 12:12:10 -0500

    Agregar archivos de proyecto.

---
+++ MiLectordeVoz/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MiLectordeVoz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBDE = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnPausa = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.btnPausa);
+            this.panel1.Controls.Add(this.btnDetener);
+            this.panel1.Controls.Add(this.btnBDE);
+            this.panel1.Controls.Add(this.btnPlay);
+            this.panel1.Location = new System.Drawing.Point(-3, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(805, 87);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPlay.BackgroundImage")));
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnPlay.Location = new System.Drawing.Point(15, 11);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(86, 65);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBDE
+            // 
+            this.btnBDE.BackColor = System.Drawing.Color.Transparent;
+            this.btnBDE.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBDE.BackgroundImage")));
+            this.btnBDE.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBDE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBDE.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnBDE.Location = new System.Drawing.Point(107, 11);
+            this.btnBDE.Name = "btnBDE";
+            this.btnBDE.Size = new System.Drawing.Size(86, 65);
+            this.btnBDE.TabIndex = 1;
+            this.btnBDE.UseVisualStyleBackColor = false;
+            this.btnBDE.Click += new System.EventHandler(this.btnBDE_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.Color.Transparent;
+            this.btnDetener.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDetener.BackgroundImage")));
+            this.btnDetener.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDetener.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetener.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnDetener.Location = new System.Drawing.Point(291, 11);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(86, 65);
+            this.btnDetener.TabIndex = 2;
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnPausa
+            // 
+            this.btnPausa.BackColor = System.Drawing.Color.Transparent;
+            this.btnPausa.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPausa.BackgroundImage")));
+            this.btnPausa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPausa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPausa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnPausa.Location = new System.Drawing.Point(199, 11);
+            this.btnPausa.Name = "btnPausa";
+            this.btnPausa.Size = new System.Drawing.Size(86, 65);
+            this.btnPausa.TabIndex = 3;
+            this.btnPausa.UseVisualStyleBackColor = false;
+            this.btnPausa.Click += new System.EventHandler(this.btnPausa_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button5.Location = new System.Drawing.Point(383, 11);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 65);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightYellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-3, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 364);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lector De Texto Avanzado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnPausa;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnBDE;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
